9c9324c50be405c9d3399f4f5cb3b2e57f7a08fb
MarcoPicca <il96marco@gmail.com>
2023-11-10 17:38:43 +0100

    aggiunto parseInt

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
-let kmUser = prompt('Inserire il totale dei km da percorrere');
-let agePassenger = prompt("Inserire l'età del passeggero");
+let kmUser = parseInt( prompt('Inserire il totale dei km da percorrere'));
+let agePassenger = parseInt( prompt("Inserire l'età del passeggero"));
 
 let standardPrice = kmUser * 0.21;
 
